04285d42eb4bd0c722fb11cf944348f405a59402
rkaku <greenapple728@icloud.com>
2019-10-15 14:23:12 +0900

    replace type names with constants

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { DELETE_EVENT } from '../actions';
 
 const Event = ({ dispatch, event }) => {
   const id = event.id;
@@ -7,7 +8,7 @@ const Event = ({ dispatch, event }) => {
 
     if (result) {
       dispatch({
-        type: "DELETE_EVENT",
+        type: DELETE_EVENT,
         id,
       });
     }

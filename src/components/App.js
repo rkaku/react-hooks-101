fda1f905449c68a2a220c908a8424a1a88c1fa7e
rkaku <greenapple728@icloud.com>
2019-10-15 18:20:45 +0900

    cerate operationLogs reducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React, { useReducer } from "react";
-import reducer from "../reducers";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import EventForm from './EventForm';
 import Events from './Events';
 import AppContext from '../contexts/AppContext';
+import reducer from "../reducers";
 
 const App = () => {
   const initialState = {
-    events: []
+    events: [],
+    operationLogs: []
   }
   const [state, dispatch] = useReducer(reducer, initialState);
 

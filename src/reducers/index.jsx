dbdb13363a34e9706abd79949ae70ef3da9baeb3
rkaku <greenapple728@icloud.com>
2019-10-19 20:26:43 +0900

    create reducer for events

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,17 +1,27 @@
+// Events Reducer
 const events = (state = [], action) => {
+  // Action Type
   switch (action.type) {
+    // Create
     case "CREATE_EVENT":
+      // New Event -> Title, Body
       const event = { title: action.title, body: action.body };
+      // Events Length
       const length = state.length;
+      // Additional ID
       let id = length === 0 ? 1 : state[length - 1].id + 1;
+      // Update Events
       return [...state, { id, ...event }];
 
+    // Delete
     case "DELETE_EVENT":
       return state;
 
+    // Delete All
     case "DELETE_ALL_EVENT":
       return [];
 
+    // Default
     default:
       return state;
   }

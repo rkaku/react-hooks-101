11bfe972fa779369bdf995c3b107a11a4a0ae814
rkaku <greenapple728@icloud.com>
2019-10-19 17:57:44 +0900

    multiple states

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
 import React, { useState } from 'react'
 
+
+// App Component Function
 const App = props => {
 
+  // useState -> Name, Price
   const [name, setName] = useState(props.name)
   const [price, setPrice] = useState(props.price)
 
+  // Reset Method
   const reset = () => {
+    // Set -> Default Props
     setPrice(props.price)
     setName(props.name)
   }
 
+  // App Component
   return (
     <>
+      {/* Display */ }
       <p>現在の{ name }は、{ price }円です。</p>
+      {/* Price Button */ }
       <button onClick={ () => setPrice(price + 1) }>+1</button>
       <button onClick={ () => setPrice(price - 1) }>-1</button>
       <button onClick={ reset }>Reset</button>
+      {/* Name Input */ }
       <input value={ name } onChange={ event => setName(event.target.value) } />
     </>
   )
 }
 
+
+// Default Props -> App Component
 App.defaultProps = {
   name: '？？？',
-  price: 1000
+  price: 0
 }
+
 
 export default App
